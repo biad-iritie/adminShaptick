14f18c5cbbeabd5094a50e3407cd4c7c14e97b7d
unknown <ziewilliamscoulibaly@gmail.com>
2018-09-06 19:37:16 +0000

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
+import { AppComponent } from './app.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewComponent  } from './newpage/new.component';
+import { EventComponent  } from './evenements/event.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NewComponent,
+    EventComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ComponentsModule,
+RouterModule,
+    AppRoutingModu
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewComponent  } from './newpage/new.component';
+import { EventComponent  } from './evenements/event.component';
+
+const routes: Routes =[
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'new',            component: NewComponent },
+    { path: 'event',          component: EventComponent },
+    { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    Com
